634854e3aef2238ca9d944ca470dd3aa0685fb50
Antoine Buirey <91411073+AntoineBuirey@users.noreply.github.com>
2025-12-02 09:02:08 +0100

    Refactor performance test to use generator for random point generation and update parameter IDs

--- tests/tests_performance.py
+++ tests/tests_performance.py
@@ -1,5 +1,6 @@
 import pytest
 from random import randint
+from typing import Iterator
 import time
 
 from triangulator.triangulator import triangulate
@@ -8,21 +9,21 @@ from triangulator.data_types import Point
 from triangulator.triangles import Triangles
 
 
-def generate_random_points(num_points: int) -> list[Point]:
-    return [Point(randint(0, 10000), randint(0, 10000)) for _ in range(num_points)]
+def generate_random_points(num_points: int) -> Iterator[Point]:
+    for _ in range(num_points):
+        yield Point(randint(0, 10000), randint(0, 10000))
 
 
 @pytest.mark.performance
 @pytest.mark.parametrize("num_points", [3, 100, 1_000, 10_000, 100_000, 1_000_000],
-                         ids=["minimal", "xs", "s", "m", "l", "xl"])
+                         ids=["MIN", "XS", "S", "M", "L", "XL"])
 def test_triangulate_large_pointset(num_points: int):
     # Generate a large PointSet with num_points points
     points = PointSet(generate_random_points(num_points))
     # Measure the time taken to triangulate
     start_time = time.time()
     triangles = triangulate(points)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    elapsed_time = time.time() - start_time
     
     print(f"Triangulated {num_points} points in {elapsed_time:.2f} seconds.")
     assert isinstance(triangles, Triangles)
